c9da021c3aef6eb9c832054a43d1e7dbc33708ec
hraka <laqahraca@protonmail.com>
2020-03-25 19:30:00 +0900

    글 추가 양식 변경

--- create_process.php
+++ create_process.php
@@ -1,4 +1,4 @@
 <?php
-file_put_contents('data/'.$_POST['word_name'], '+'.$_POST['meaning']);
+file_put_contents('data/'.$_POST['word_name'], $_POST['meaning']);
 header('Location: /index.php?word='.$_POST['word_name']);
 ?>
