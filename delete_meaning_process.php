42d94bf6160f96b67618a3e819715da58ddc9b3b
hraka <laqahraca@protonmail.com>
2020-06-04 02:25:19 +0900

    기능추가: 의미 별 삭제 기능
    - meaning 테이블에서 데이터를 삭제한다.

---
+++ delete_meaning_process.php
@@ -0,0 +1,26 @@
+<?php
+
+$conn = mysqli_connect(
+	'localhost',
+	'root', 
+	'1111', 
+	'words');
+
+
+$sql = "
+	DELETE FROM meaning
+		WHERE id={$_POST['meaning_id']};
+";
+
+
+$result = mysqli_query($conn, $sql);
+if($result === false){
+	echo '삭제하는 과정에서 문제가 생겼습니다. 관리자에게 문의해주세요.'; //사용자에게 뜨는 것
+	error_log(mysqli_error($conn)); //관리자가 볼 수 있는 시스템 에러 메세지.
+} else {
+	echo '성공했습니다. <a href="index.php">돌아가기</a>';
+}
+echo $sql;
+
+
+?>
