2062e0f6f4e8e7b6273f63e8e65b777217643a80
hraka <laqahraca@protonmail.com>
2020-06-04 00:04:50 +0900

    입력후 입력한 내용 바로 보기(의미 업데이트시)
    - 기존의 업데이트 작업은 meaning id를 기반으로 이루어졌기에 word_id를 찾기 위한 sql문을 새로 작성했다.
    - 업데이트 작업을 한 id의 meaning 데이터를 가져오고 그 중에서 word_id를 쓴다

--- update_meaning_process.php
+++ update_meaning_process.php
@@ -22,65 +22,23 @@ $sql = "
         WHERE id={$filtered['id']};
     ";
 
-
 $result = mysqli_query($conn, $sql);
+
+
+
+$sql_for_word_id = "
+    SELECT * FROM meaning WHERE id={$filtered['id']}
+    ";
+
+$result_for_word_id = mysqli_query($conn, $sql_for_word_id);
+$row_for_word_id = mysqli_fetch_array($result_for_word_id);
+
 if($result === false){
     echo '수정하는 과정에서 문제가 생겼습니다. 관리자에게 문의해주세요.'; //사용자에게 뜨는 것
     error_log(mysqli_error($conn)); //관리자가 볼 수 있는 시스템 에러 메세지.
 } else {
-    echo '성공했습니다. <a href="index.php">돌아가기</a>';
+    echo '성공했습니다. <a href="index.php?word='.$row_for_word_id['word_id'].'">돌아가기</a>';
 }
 echo $sql;
-
-
-// function get_valid_file() {
-//     if(isset($_POST['word_name'])){
-//         return "./data/".$_POST['word_name'];
-//     } else {
-//         return null;
-//     }
-// }
-
-// function check_and_get_meaning($filename){
-//     if (file_exists($filename)) {
-//         if (is_readable($filename)) {
-//             return get_meaning_list_from_file($filename);
-//         } else {
-//             return null;
-//         }
-//     } else {
-//         return null;
-//     }
-// }
-
-// function get_meaning_list_from_file($filename){
-//     $opfile = fopen($filename, 'r');
-//     $contents = fread($opfile, filesize($filename));
-//     $meaning_explode = explode("+", $contents);
-//     fclose($opfile);
-//     return $meaning_explode;
-
-// }
-
-// function update_target_meaning_in_file($meaning_list, $target) {
-
-// 	$update_meaning = array($target => $_POST['new_meaning']);
-
-// 	$new_meaning_list = array_replace($meaning_list, $update_meaning);
-
-//     return $new_meaning_list;
-// }
-
-
-// $filename = get_valid_file();
-// $target = $_POST['meaning_num'];
-
-// $old_meaning_list = check_and_get_meaning($filename);
-// $new_meaning_list = update_target_meaning_in_file($old_meaning_list, $target);
-
-// file_put_contents($filename, implode('+', $new_meaning_list));
-// header('Location: /index.php?word='.$_POST['word_name']);
-
-
 
 ?>
