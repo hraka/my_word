961b3327ead02c427f452fea6d8bb5173db8d37a
hraka <laqahraca@protonmail.com>
2020-06-04 00:02:34 +0900

    입력후 입력한 내용 바로 보기(의미 생성시)
    - 의미 생성시에 이미 word_id 를 사용하므로 word_id를 다시 호출하여 url에 넣는다

--- create_meaning_process.php
+++ create_meaning_process.php
@@ -25,12 +25,13 @@ $sql = "
 	";
 
 
+
 $result = mysqli_query($conn, $sql);
 if($result === false){
 	echo '저장하는 과정에서 문제가 생겼습니다. 관리자에게 문의해주세요.'; //사용자에게 뜨는 것
 	error_log(mysqli_error($conn)); //관리자가 볼 수 있는 시스템 에러 메세지.
 } else {
-	echo '성공했습니다. <a href="index.php">돌아가기</a>';
+	echo '성공했습니다. <a href="index.php?word='.$filtered['word_id'].'">돌아가기</a>';
 }
 
 echo $sql;
