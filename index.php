6d0558935dbd97abcf6cd7e0b3e43f0deffe23cf
hraka <laqahraca@protonmail.com>
2020-06-05 02:34:23 +0900

    기능추가: 카테고리 가져오기
    - 카테고라이징 테이블과 워드 테이블 조인
    - GET으로 가져온 카테고리 아이디 값으로 WHERE문 추가
    
    - 카테고리 아이디 값이 GET에 없거나 0보다 작을 경우 (문자로 시작하는 값도 해당됨) 카테고리 값이 없는 것으로 판단
    - 카테고리 값이 없는 경우 워드 테이블에서 단어 목록 가져오기
    
    - 변수명을 특정함 (sql_word_list, sql_category_list)
    
    - 단어 목록 가져올 때 SELECT * 대신 SELECT id, word_name 으로 변경 - 내부 연산이 더 간단해지길 바랬다.

--- index.php
+++ index.php
@@ -6,18 +6,37 @@ $conn = mysqli_connect(
 	'words');
 
 
-$sql = "SELECT * FROM word LIMIT 1000";
-$result = mysqli_query($conn, $sql);
-
-$list = '';
-while($row = mysqli_fetch_array($result)) {
-	$escaped_title = htmlspecialchars($row['word_name']); //목록에 들어갈 단어명
-	$list = $list."<li><a href=\"index.php?word={$row['id']}\">{$escaped_title}</a></li>"; //단어 id를 GET으로 취하는 url 연결
-}
 
 $word_info = array(
 	'word_name' => 'Welcome',
 	'profile' => 'Hello, This is your Dictionary');
+
+$category_list = "<li><a href=\"index.php?\">전체</a></li>";
+
+$sql_category_list = "SELECT id, category_name FROM category LIMIT 1000";
+$result_category_list = mysqli_query($conn, $sql_category_list);
+while($row_category_list = mysqli_fetch_array($result_category_list)) {
+	$escaped_category = htmlspecialchars($row_category_list['category_name']);
+	$category_list = $category_list."<li><a href=\"index.php?category={$row_category_list['id']}\">{$escaped_category}</a></li>";
+}
+
+$sql_word_list = "SELECT id, word_name FROM word LIMIT 1000";
+$filtered_category_id = 0;
+
+if(isset($_GET['category']) && $_GET['category'] > 0) {
+	$filtered_category_id = mysqli_real_escape_string($conn, $_GET['category']);
+	$sql_word_list = "SELECT id, word_name FROM categorizing LEFT JOIN word ON categorizing.word_id = word.id WHERE category_id ={$filtered_category_id}"; //*보다 word_name 으로 컬럼명 특정하는게 나을까? 어차피 category_id를 색인하느라 여러 컬럼을 다 읽는 거 아닐까? 가져오는 건 또 다른가? 어디에 쓰는 것도 아닌데도? fetch과정에서 다른가? result에 들어가는 field count 값이 다르다.
+}
+
+$result_word_list = mysqli_query($conn, $sql_word_list);
+
+$list = '';
+while($row_word_list = mysqli_fetch_array($result_word_list)) {
+	$escaped_title = htmlspecialchars($row_word_list['word_name']); //목록에 들어갈 단어명
+	$list = $list."<li><a href=\"index.php?category={$filtered_category_id}&word={$row_word_list['id']}\">{$escaped_title}</a></li>"; //단어 id를 GET으로 취하는 url 연결
+}
+
+
 
 $printing_meanings = '';
 
@@ -86,7 +105,7 @@ if(isset($_GET['word'])) { //word id를 받는다.
 		</header>	
 		<div id="content">
 			<nav id="categories">
-				<label><strong>카테고리</strong>
+				<label><strong>카테고리 <?=$category_list?></strong>
 					<ul>
 						<?=$list?>
 					</ul>
