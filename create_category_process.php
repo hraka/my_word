6c5319417d6e6e04fa9e7bd915bb12533fa87279
hraka <laqahraca@protonmail.com>
2020-06-07 03:37:39 +0900

    기능추가: 카테고리 생성
    - 카테고리를 생성하는 페이지를 만들었다.
    - 추후 자바스크립트를 통해 팝업창으로 구현해도 될 것이다.
    
    #1

---
+++ create_category_process.php
@@ -0,0 +1,40 @@
+<?php
+
+error_reporting(E_ALL);
+
+ini_set("display_errors", 1);
+
+$conn = mysqli_connect(
+	'localhost',
+	'root', 
+	'1111', 
+	'words');
+
+$filtered = array(
+	'category_name' => mysqli_real_escape_string($conn, $_POST['category_name']),
+);
+
+$sql = "
+	INSERT INTO category
+		(category_name, created)
+		VALUES(
+			'{$filtered['category_name']}',
+			NOW()
+		)
+	";
+$result = mysqli_query($conn, $sql);
+
+if($result === false){
+	echo '저장하는 과정에서 문제가 생겼습니다. 관리자에게 문의해주세요.'; //사용자에게 뜨는 것
+	error_log(mysqli_error($conn)); //관리자가 볼 수 있는 시스템 에러 메세지.
+} else {
+	echo '성공했습니다. <a href="index.php">돌아가기</a>';
+
+	sleep(2);
+	header("Location : index.php");
+
+}
+echo $sql;
+// file_put_contents('data/'.$_POST['word_name'], $_POST['meaning']);
+// header('Location: /index.php?word='.$_POST['word_name']);
+?>
