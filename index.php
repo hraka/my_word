c87a5b1c638c8a803ac9154cc20e932eb9f3de86
hraka <laqahraca@protonmail.com>
2020-07-22 02:20:53 +0900

    기능추가: 검색기능
    1. 코드에 이름을 붙여 알아보기 쉽도록 함수화 했다
    2. 검색 기능을 넣었다.
     - 검색 시 네비게이션의 단어목록과 본문의 단어목록이 검색 결과로 나온 데이터로 채워진다.
    - 카테고리 선택 시에는 카테고리 선택이 우선한다. (else if 사용)
    3.  네비게이션의 단어목록과 본문의 단어목록이 연동되도록 같은 sql문을 사용하게 했다. 다만 'LIMIT n '  부분을 따로 합성하여 네이게이션과 본문에 뜨는 목록의 최대갯수가 달라지게 했다.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $conn = mysqli_connect(
 	'words');
 
 $word_info = array(
-	'word_name' => 'Welcome',
+	'word_name' => '전체 단어 보기',
 	'profile' => 'Hello, This is your Dictionary');
 
 $category_list = "<li><a href=\"index.php?\">전체</a></li>";
@@ -15,18 +15,37 @@ $selected_category = '';
 $list = '';
 $printing_meanings = '';
 $printing_synonyms = '';
-$cat_word_list = '';
+$card_list = '';
 
 
-$sql_category_list = "SELECT id, category_name FROM category LIMIT 1000";
-$result_category_list = mysqli_query($conn, $sql_category_list);
 
-while($row_category_list = mysqli_fetch_array($result_category_list)) {
-	$escaped_category = htmlspecialchars($row_category_list['category_name']);
-	$category_list = $category_list."<li><a href=\"index.php?category={$row_category_list['id']}\">{$escaped_category}</a></li>";
+
+
+function get_category_list($conn) {
+
+	$sql_category_list = "SELECT id, category_name FROM category LIMIT 1000";
+	$result_category_list = mysqli_query($conn, $sql_category_list);
+
+	while($row_category_list = mysqli_fetch_array($result_category_list)) {
+		$escaped_category = htmlspecialchars($row_category_list['category_name']);
+		$category_list = $category_list."<li><a href=\"index.php?category={$row_category_list['id']}\">{$escaped_category}</a></li>";
+	}
+
+	return $category_list;
 }
+$category_list = get_category_list($conn);
 
-$sql_word_list = "SELECT id, word_name FROM word ORDER BY word_name LIMIT 1000";
+// function Make_List($result, $attribute1, $attribute2, $text) {
+// 	while($row = mysqli_fetch_array($result)) {
+// 		$escaped_attribute1 = htmlspecialchars($row[$attribute1]);
+// 		$escaped_attribute2 = htmlspecialchars($row[$attribute2]);
+// 		$list = $list.
+// 	}
+// }
+
+$show_num = 1000;
+
+$sql_word_list = "SELECT id, word_name FROM word ORDER BY word_name";
 $filtered_category_id = 0;
 
 if(isset($_GET['category']) && $_GET['category'] > 0) {
@@ -41,42 +60,66 @@ if(isset($_GET['category']) && $_GET['category'] > 0) {
 	$word_info['word_name'] = $selected_category.' 카테고리';
 
 }
+else if(isset($_GET['search'])){
+	$filtered_search = mysqli_real_escape_string($conn, $_GET['search']);
+	$search_keyword = $filtered_search;
+	$sql_word_list = "SELECT id, word_name FROM word WHERE word_name LIKE '%{$search_keyword}%'";
 
-$result_word_list = mysqli_query($conn, $sql_word_list);
+	$word_info['word_name'] = $search_keyword.' 검색 결과';
+	$word_info['profile'] = '검색 결과 입니다.';
+}
+
+$result_word_list = mysqli_query($conn, $sql_word_list." LIMIT {$show_num}");
 
 while($row_word_list = mysqli_fetch_array($result_word_list)) {
 	$escaped_word = htmlspecialchars($row_word_list['word_name']);
+	$filtered_word_id = htmlspecialchars($row_word_list['id']);
 	$list = $list."<li><a href=\"index.php?category={$filtered_category_id}&word={$row_word_list['id']}\">{$escaped_word}</a></li>";
-
-	$cat_word_list = $cat_word_list."<span>{$escaped_word} </span>";
 }
 
-if(isset($_GET['category']) && $_GET['category'] > 0) {
-	$word_info['profile'] = $cat_word_list."
-		<form>
-			<input type='text' class='mini_text'>
-			<input type='submit' class='btn' value='추가'>
-		</form>
-		";
+
+
+function show_word_card($sql, $conn, $show_num){
+	// $sql = "SELECT id, word_name FROM word ORDER BY id DESC LIMIT {$show_num}";
+
+	// if($filtered_category_id > 0) {
+	// 	$sql = "SELECT id, word_name FROM categorizing LEFT JOIN word ON categorizing.word_id = word.id WHERE category_id ={$filtered_category_id} ORDER BY id DESC LIMIT {$show_num}";
+	// }
+
+	$result = mysqli_query($conn, $sql." LIMIT {$show_num}");
+	$card_list = '';
+	while($row = mysqli_fetch_array($result)) {
+		$escaped_word = htmlspecialchars($row['word_name']);
+		$filtered_word_id = htmlspecialchars($row['id']);
+
+		$card_list = $card_list."
+			<a href='index.php?category={$filtered_category_id}&word={$filtered_word_id}'><article>
+				<form method=\"post\" class=\"on_off\" action=\"\">
+						<label class=\"switch\">
+							<input type=\"checkbox\" name=\"on_off\" id=\"on_off\"value=\"눌렸어\"onclick=\"\" onChange=\"this.form.submit()\">
+							<span class=\"slider round\"></span>
+							<input type=\"hidden\" name=\"m_id\" value={$escaped_meaning_id}>
+							<input type=\"hidden\" name=\"category_id\" value={$filtered_category_id}>
+							<input type=\"hidden\" name=\"word_id\" value={$filtered_word_id}>
+							<input type=\"hidden\" name=\"now_onoff\" value={$escaped_onoff}>
+						</label>
+					</form>
+				{$escaped_word}
+			</article></a>";
 	}
 
-/*function testfun2($m_id)
-{
-   echo "Your test function on button click is working";
+	return $card_list;
 }
 
-if(array_key_exists('on_off_work',$_POST)){
-   testfun2($_POST['on_off_work']);
-}*/
+$card_list = show_word_card($sql_word_list, $conn, 15);
 
-if(isset($_GET['word'])) { //word id를 받는다.
-	$filtered_word_id = mysqli_real_escape_string($conn, $_GET['word']); //word_info와 통합시킬까?ㄴ
-	$sql_word = "SELECT * FROM word WHERE id=\"{$filtered_word_id}\"";
-	$result_word = mysqli_query($conn, $sql_word);
-	$row_word = mysqli_fetch_array($result_word);	
-	$word_info['word_name'] = htmlspecialchars($row_word['word_name']);
-	$word_info['profile'] = htmlspecialchars($row_word['profile']);
 
+if(isset($_GET['category']) && $_GET['category'] > 0) {
+	$word_info['profile'] = '카테고리를 선택했습니다';
+	}
+
+function show_synonym_list($filtered_word_id, $conn) {
+	$printing_synonyms = '';
 	$sql_synonym = "SELECT id, word_name FROM relation_synonym LEFT JOIN word ON relation_synonym.obj_word_id = word.id WHERE sub_word_id =\"{$filtered_word_id}\"";
 	$result_synonym = mysqli_query($conn, $sql_synonym);
 	while ($row_synonym = mysqli_fetch_array($result_synonym)) {
@@ -84,19 +127,22 @@ if(isset($_GET['word'])) { //word id를 받는다.
 		$escaped_synonym_id = htmlspecialchars($row_synonym['id']);
 		$printing_synonyms = $printing_synonyms." <a href=\"index.php?category={$filtered_category_id}&word={$escaped_synonym_id}\">[".$escaped_synonym."]</a>";
 	}
+	return $printing_synonyms;
+}
 
+function show_meaning_card($filtered_word_id, $conn){
+	$printing_meanings = '';
+	$sql = "SELECT * FROM meaning WHERE word_id={$filtered_word_id} ORDER BY created DESC";
+	$result = mysqli_query($conn, $sql);
 
-	$sql_meaning = "SELECT * FROM meaning WHERE word_id={$filtered_word_id} ORDER BY created DESC";
-	$result_meaning = mysqli_query($conn, $sql_meaning);
-
-	while($row_meaning = mysqli_fetch_array($result_meaning)) {
-		$escaped_meaning = nl2br(htmlspecialchars($row_meaning['meaning']));
-		$escaped_meaning_id = htmlspecialchars($row_meaning['id']);
-		$escaped_time = htmlspecialchars($row_meaning['created']);
+	while($row = mysqli_fetch_array($result)) {
+		$escaped_meaning = nl2br(htmlspecialchars($row['meaning']));
+		$escaped_meaning_id = htmlspecialchars($row['id']);
+		$escaped_time = htmlspecialchars($row['created']);
 		$escaped_source = '';
-		$escaped_onoff = htmlspecialchars($row_meaning['onoff']);
-		if($row_meaning['source'] !== NULL) {
-			$escaped_source = '출처 : '.htmlspecialchars($row_meaning['source']);
+		$escaped_onoff = htmlspecialchars($row['onoff']);
+		if($row['source'] !== NULL) {
+			$escaped_source = '출처 : '.htmlspecialchars($row['source']);
 		}
 
 		$printing_off_class = '';
@@ -144,7 +190,33 @@ if(isset($_GET['word'])) { //word id를 받는다.
 			</article>
 		";
 	}
+	return $printing_meanings;
 }
+
+if(isset($_GET['word'])) { //word id를 받는다.
+	$filtered_word_id = mysqli_real_escape_string($conn, $_GET['word']); //word_info와 통합시킬까?ㄴ
+	$sql_word = "SELECT * FROM word WHERE id=\"{$filtered_word_id}\"";
+	$result_word = mysqli_query($conn, $sql_word);
+	$row_word = mysqli_fetch_array($result_word);	
+	$word_info['word_name'] = htmlspecialchars($row_word['word_name']);
+	$word_info['profile'] = htmlspecialchars($row_word['profile']);
+
+	$printing_synonyms = show_synonym_list($filtered_word_id, $conn);
+	$printing_meanings = show_meaning_card($filtered_word_id, $conn);
+}
+
+$printing_world = '';
+$sql = "SELECT id, world_name FROM world";
+$result = mysqli_query($conn, $sql);
+while($row = mysqli_fetch_array($result)) {
+	$escaped_world = htmlspecialchars($row['world_name']);
+	$printing_world = $printing_world."
+		<li>{$escaped_world}</li>";
+}
+
+
+
+
 ?>
 
 <!doctype html>
@@ -172,10 +244,14 @@ if(isset($_GET['word'])) { //word id를 받는다.
 		<div id="content">
 			<div id="left_content">
 
-				<form>
+				<form action="index.php" method="get">
 					<input type="text" name="search">
 					<input type="submit" name="" value="검색">
 				</form>
+
+				<ul>
+					<?=$printing_world?>
+				</ul>
 				<nav id="categories">
 					<button onclick="
 						document.getElementById('categories').style.display='none';
@@ -190,6 +266,13 @@ if(isset($_GET['word'])) { //word id를 받는다.
 						</ul>
 						<input type="button" class="btn" value="카테고리 설정" onclick = "location.href = 'category_set.php'">
 
+						<form>
+							<input type="hidden" name="world" value="<?=$_POST['world']?>">
+							<input type="submit" value="단어 만들기">
+						</form>
+
+						<input type="button" class="btn" name="만들기" value="단어 만들기" onclick = "location.href = 'create.html'">
+
 						<ul class="word_list">
 							<?=$list?>
 						</ul>
@@ -201,8 +284,6 @@ if(isset($_GET['word'])) { //word id를 받는다.
 				">
 					카테고리 펼치기
 				</button>
-
-				<input type="button" class="btn" name="만들기" value="단어 만들기" onclick = "location.href = 'create.html'">
 			</div>
 			<div id="set">
 						    
@@ -240,7 +321,12 @@ if(isset($_GET['word'])) { //word id를 받는다.
                    	<?php
                    } else {
                    	?>
-               	</div>                  
+               	</div>
+               		<?php
+               			if(!isset($_GET['word'])) {
+               				echo ($card_list);
+               			}
+               		?>                
                    <?php
                		}
                    ?>
