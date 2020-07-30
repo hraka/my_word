3acf1901d3af240fe61c8e5e71c40629d17d4251
hraka <laqahraca@protonmail.com>
2020-07-30 23:34:03 +0900

    기능: 세계 기능 추가
    - 세계란 각 단어마다 하나씩 갖을 수 있게 한 카테고리다.
    - 세계 생성 페이지를 만들었다.
    -

---
+++ create_world.php
@@ -0,0 +1,49 @@
+
+<!doctype html>
+<html>
+	<head>
+		<title>언어 사전</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="switch.css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nanum+Myeongjo|Noto+Sans+KR&display=swap" >
+		<link rel="stylesheet" href="style_mobile.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+
+	<body>
+		<div>
+		
+			<h1><a href="index.php">언어 사전</a></h1>
+			
+			
+			<div>
+
+				<div id="set">
+				
+				    
+				
+					<h2>새로운 세계를 만듭니다
+                    </h2>
+
+                        
+                    
+                    <form action="create_world_process.php" method="post">
+                        
+                        <p>
+ 	                       <input type="text" name="world_name" placeholder="세계 이름">
+
+                        </p>
+
+                        <p>	
+                        	<input type="submit">
+                        </p>
+                    </form>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
+
+
+
---
+++ create_world_process.php
@@ -0,0 +1,37 @@
+<?php
+
+error_reporting(E_ALL);
+
+ini_set("display_errors", 1);
+
+$conn = mysqli_connect(
+	'localhost',
+	'root', 
+	'1111', 
+	'words');
+
+$filtered = array(
+	'world_name' => mysqli_real_escape_string($conn, $_POST['world_name']),
+);
+
+$sql = "
+	INSERT INTO world
+		(world_name, created)
+		VALUES(
+			'{$filtered['world_name']}',
+			NOW()
+		)
+	";
+$result = mysqli_query($conn, $sql);
+
+if($result === false){
+	echo '저장하는 과정에서 문제가 생겼습니다. 관리자에게 문의해주세요.'; //사용자에게 뜨는 것
+	error_log(mysqli_error($conn)); //관리자가 볼 수 있는 시스템 에러 메세지.
+} else {
+	echo '성공했습니다. <a href="index.php">돌아가기</a>';
+
+	sleep(2);
+	header("Location: index.php");
+
+}
+?>
